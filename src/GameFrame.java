3d5c7c8fa3beeeeeac8a74b7dae11f0ee95275c7
ShreyaSriAN <75601363+ShreyaSriAN@users.noreply.github.com>
2020-12-11 21:44:58 +0530

    Add files via upload

---
+++ src/GameFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{ //Inheritance
+    private static final long serialVersionUID = 1L;
+
+    GameFrame(){ //Constructor
+
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+}
